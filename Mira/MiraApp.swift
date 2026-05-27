bb976f75425806dca755577f54756ef1f2f38d64
Curnic Dorin <157069618+C-Dorin@users.noreply.github.com>
2026-05-27 16:14:30 +0300

    Initial Commit

---
+++ Mira/MiraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiraApp.swift
+//  Mira
+//
+//  Created by Dorin Curnic on 27.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct MiraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
